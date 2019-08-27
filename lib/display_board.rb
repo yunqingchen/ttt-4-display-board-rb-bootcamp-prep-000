35b7b7d01ea2f847405a8f0c12d6560bb35d7bca
Clark Chen <yyunqingchenn@gmail.com>
2019-08-27 06:53:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 
-display_board(board)
+def (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
